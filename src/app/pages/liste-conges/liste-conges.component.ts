63fc19a5016e116de42f6b66fae05fec622d971c
Sebastien Rigot <sebastien.rigot@gmail.com>
2025-10-29 21:02:14 +0000

    refactor : transforme liste conges en signal

--- src/app/pages/liste-conges/liste-conges.component.ts
+++ src/app/pages/liste-conges/liste-conges.component.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { CongesService } from 'src/app/services/conges.service';
-import { Conges } from 'src/app/models/conges';
-import { Observable } from 'rxjs';
+import { Component, inject, OnInit, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs/operators';
+import { Conges } from 'src/app/models/conges';
+import { CongesService } from 'src/app/services/conges.service';
 
 interface AffichageConges extends Conges {
   totalJours: number;
 }
 
 @Component({
-    selector: 'app-liste-conges',
-    templateUrl: './liste-conges.component.html',
-    styleUrls: ['./liste-conges.component.scss'],
-    standalone: false
+  selector: 'app-liste-conges',
+  templateUrl: './liste-conges.component.html',
+  styleUrls: ['./liste-conges.component.scss'],
+  standalone: false
 })
 export class ListeCongesComponent implements OnInit {
-  displayedColumns: string[] = ['dateDebut', 'dateFin', 'joursPris', 'actions'];
-  public listeConges: Observable<AffichageConges[]>;
+  private _congesService = inject(CongesService);
 
-  constructor(congesService: CongesService) {
-    this.listeConges = congesService.getAllConges().pipe(
+  displayedColumns: string[] = ['dateDebut', 'dateFin', 'joursPris', 'actions'];
+  public listeConges: Signal<AffichageConges[]>;
+
+  constructor() {
+    this.listeConges = toSignal(this._congesService.getAllConges().pipe(
       map(conges => conges.map(conge => ({ ...conge, totalJours: conge.joursPris.reduce((a, b) => a + b.nombreJours, 0) })))
-    );
+    ));
   }
 
   ngOnInit() {
